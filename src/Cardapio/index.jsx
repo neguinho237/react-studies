787a0b695ab53fd21007918fcc53389d0b930d84
neguinho237 <brunomirands237@gmail.com>
2024-07-18 16:01:56 -0300

    Cardapio

--- src/Cardapio/index.jsx
+++ src/Cardapio/index.jsx
@@ -108,9 +108,25 @@ export default function Cardapio (){
                 </div>
                 </div>
 
+                <div className="bg-black/60 hidden items-center justify-center w-full h-full fixed top-0 left-0 z-[99]">
+                    <div className=" bg-white p-5 rounded-md min-w-[90%] md:min-w-[600px]">
+                        <h2 className="text-center font-bold text-2xl mb-2">Meu carrinho</h2>
+                        <div id="cart-items" className="flex justify-between mb-2 flex-col">
+                            <p className="font-bold">Total: <span id="cart-total">0.00</span></p>
+                            <p className="font-bold">Endereço de entrega</p>
+                            <input type="text" placeholder="Digite seu endereço completo..." id="address" className="w-full border-2 p-1 rounded my-1" />
+                            <p className="text-red-500 hidden" id="address-warn">Digite seu endereço completo!</p>
+                            <div className="flex items-center justify-between mt-5 w-full">
+                                <button id="close-modal-btn">Fechar</button>
+                                <button id="checkout-btn" className="bg-green-500 text-white px-4 py-1 rounded">Finalizar pedido</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
                 <footer className="w-full bg-red-500 py-3 fixed bottom-0 z-40 flex items-center justify-center">
                     <button id="cart-btn" className="flex gap-2 items-center font-bold text-white">
-                       (<span id="cart-count">2</span>)
+                       (<span id="cart-count">0</span>)
                         Veja meu carrinho 
                         <img className="w-7" src={Carrrinho} alt="" />
                     </button>
