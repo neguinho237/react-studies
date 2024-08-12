48183b69ada571160dc9465d6ef767ab4bdcb140
neguinho237 <brunomirands237@gmail.com>
2024-08-12 17:25:56 -0300

    Cars

--- src/CarroApp/index.jsx
+++ src/CarroApp/index.jsx
@@ -3,7 +3,22 @@ import "./styles.css"
 export default function Carros (){
     return(
         <body>
-             
+             <section className="header" id="header">
+                <div className="navbar">
+                    <div className="logo">
+                        <a href="#">Chevrolet</a>
+                    </div>
+                    <div className="menu">
+                        <ul>
+                            <li><a href="#" className="active">Carros</a></li>
+                            <li><a href="#">História</a></li>
+                            <li><a href="#">Técnologia</a></li>
+                            <li><a href="#">Produção</a></li>
+                            <li><a href="#">Reviews</a></li>
+                        </ul>
+                    </div>
+                </div>
+             </section>
         </body>
     )
 }
