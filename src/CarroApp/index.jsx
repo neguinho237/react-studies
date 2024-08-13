3f81c5774f6bea3bd87705a428423b9b078c9cc4
neguinho237 <brunomirands237@gmail.com>
2024-08-13 20:13:48 -0300

    Cars

--- src/CarroApp/index.jsx
+++ src/CarroApp/index.jsx
@@ -1,5 +1,7 @@
 import "./styles.css"
-
+import Facebook from "./src/assets/images/Facebook.svg"
+import Instagram from "./src/assets/images/Instagram.svg"
+import X from "./src/assets/images/X.svg"
 export default function Carros (){
     return(
         <body>
@@ -17,7 +19,26 @@ export default function Carros (){
                             <li><a href="#">Reviews</a></li>
                         </ul>
                     </div>
+                    <div className="social-media">
+                        <ul>
+                           <li><a href="https://pt-br.facebook.com/"><img src={Facebook} alt="" /></a></li>
+                           <li><a href="https://www.instagram.com/ " ><img src={Instagram} alt="" /></a></li>
+                           <li><a href="https://x.com/home"><img src={X} alt="" /></a></li>
+                        </ul>
+                    </div>   
                 </div>
+                <div className="hero">
+                    <div className="row">
+                        <div className="left-sec">
+                            <div className="content">
+                                <h2><span>chevrolet</span>camaro</h2>
+                                <p>It is a long established fact that a set reader will distracted by the readable content of a page when looking at its layout technology.</p>
+                            </div>
+                            <button className="discover-btn">Descobrir</button>
+                        </div>
+                    </div>
+                </div>
+
              </section>
         </body>
     )
