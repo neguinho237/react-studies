1e5825017ff68803b9d9be1224e9419a06acd3a1
neguinho237 <brunomirands237@gmail.com>
2024-08-22 16:40:03 -0300

    feat:Header Atualizado

--- src/components/Welcome/index.jsx
+++ src/components/Welcome/index.jsx
@@ -4,15 +4,17 @@ export default function Welcome(){
     return(
         <div className="Container ">
             <div className="Header ">
-                <div className="flex justify-between">
-                    <div className="flex gap-3 ">
+                <div className="flex justify-between  mt-16">
+                    <div className="flex ml-5 gap-4 ">
                     <img src={Symbol} alt="" />
-                    <h1 className = "mt-4">Relume</h1>
+                    <h1 className="mt-3">Relume</h1>
                     </div>
-                    <li><a className="text-slate-950" href="">relume</a></li>
+                    <li className="mt-4"><a className="text-slate-950 mr-7 " href="">relume.io</a></li>
                 </div>
-                <h1>O rato roeu a roupa do rei de Roma</h1>
-                <p>We`re stoked you`re here! Here`s some important info to get started.</p>
+                <div className="flex-col">
+                <h1 className="text-5xl font-bold ml-6 mt-20">Welcome to the Relume Library Figma Kit</h1>
+                <p className="ml-7 mt-6">We`re stoked you`re here! Here`s some important info to get started.</p>
+                </div>
             </div>
         </div>
     )
