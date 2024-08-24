9c11b45bf3e71980f2f28803200571b6e89ace6c
neguinho237 <brunomirands237@gmail.com>
2024-08-24 12:42:44 -0300

    feat: WelcomeAtualizado

--- src/components/Welcome/index.jsx
+++ src/components/Welcome/index.jsx
@@ -17,6 +17,18 @@ export default function Welcome(){
                 </div>
             </div>
             <div className="Main">
+                <div className="flex gap-16">
+                    <div className="mt-16 ml-6">
+                <div className="linha max-w-3xl h-1 bg-slate-300 "></div>
+                <div>
+                    <h1 className="text-4xl font-bold mt-12">About</h1>
+                    <p className="text-lg text-gray-500 mt-6">This Figma Library built the community for the community.</p>
+                    <p className="text-lg text-gray-500 mt-6 max-w-3xl h-36">After seeing many request for a Figma version of Relume Library the world's largest webflow component library the team minimal version
+                        took it upon themselves to build it. not only have they gone an epic job of building 900+ pixel-perfect Figma components
+                    </p>
+                    <h1 className="text-2xl font-semibold">Why did we build this?</h1>
+                </div>
+                </div>
                 <div className="mt-16 Quadrado w-96 h-96 bg-gray-300 mt rounded" >
                     <div className="flex mt-3">
                     <h1 className="text-2xl font-semibold flex ml-3 mt-3 ">Creators</h1>
@@ -27,6 +39,7 @@ export default function Welcome(){
                     </div>
                     <p className=" flex h-24 w-80 mt-5">Built in partnership with the team at Minimal Square</p>
                     </div>
+                </div>
                 </div>
             </div>
         </div>
